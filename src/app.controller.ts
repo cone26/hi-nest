b8df26eb981031a05828605d3ecfea9061bb47db
SiwonHwang <sight41siyah@gmail.com>
2022-09-30 17:34:20 +0900

    architecture - class

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// controller == express의 router
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // express의 get router와 같은 역할
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+  // 데코레이터
+  @Get('/hello')
+  sayHello(): string {
+    // 함수
+    return 'Hello everyone';
+  }
+  // 데코레이터는 그 데코레이터를 꾸며주는 함수나 클래스와 붙어있어야 한다. => 공백 x
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 데코레이터 => 클래스에 함수 기능을 추가할 수 있음
+// app module == root module
+// 하나의 모듈에서 하나의 어플리케이션을 생성
 @Module({
   imports: [],
   controllers: [AppController],
